3a5422ce70e5af58e805426ccdc00b0bdc48d4a0
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-10-23 01:52:00 +0100

    Add tests for phi(n)

--- python/maths/gcd.test.py
+++ python/maths/gcd.test.py
@@ -6,3 +6,14 @@ if __name__ == '__main__':
     assert gcd(37, 600) == 1
     assert gcd(20, 100) == 20
     assert gcd(624129, 2061517) == 18913
+
+    assert phi(1) == 1
+    assert phi(2) == 1
+    assert phi(3) == 2
+    assert phi(4) == 2
+    assert phi(5) == 4
+    assert phi(6) == 2
+    assert phi(7) == 6
+    assert phi(8) == 4
+    assert phi(9) == 6
+    assert phi(10) == 4
