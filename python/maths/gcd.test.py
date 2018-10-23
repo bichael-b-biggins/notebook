d220aa7e8faf5ba581dd3479cd8ea7b1dccdd50c
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-10-23 01:48:02 +0100

    add gcd tests

---
+++ python/maths/gcd.test.py
@@ -0,0 +1,8 @@
+from gcd import *
+
+if __name__ == '__main__':
+    assert gcd(1, 1) == 1
+    assert gcd(13, 13) == 13
+    assert gcd(37, 600) == 1
+    assert gcd(20, 100) == 20
+    assert gcd(624129, 2061517) == 18913
